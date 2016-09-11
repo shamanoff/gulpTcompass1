387f777eb1db626cd176d5f8bd38479a21c994c7
michaelb <shamanoff@gmail.com>
2016-09-11 16:29:02 +0300

    work

--- js/app.js
+++ js/app.js
@@ -81,3 +81,22 @@
 
 
 }());
+
+
+$('#_biggify').on('click', function() {
+    var fontSize = $('html').css('font-size');
+    var newFontSize = parseInt(fontSize)+1;
+
+    $('html').css('font-size', newFontSize+'px')
+});
+
+$('#_smallify').on('click', function() {
+    var fontSize = $('html').css('font-size');
+    var newFontSize = parseInt(fontSize)-1;
+
+    $('html').css('font-size', newFontSize+'px')
+});
+
+$('#_reset').on('click', function() {
+    $('html').css('font-size', '1rem')
+});
